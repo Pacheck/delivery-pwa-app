4a1d9499152c507df7f6e8f1489bad3774458f1a
Rianfiore <rianfiore@gmail.com>
2022-01-22 04:23:38 -0300

    update styled components

--- src/pages/landing-page/index.tsx
+++ src/pages/landing-page/index.tsx
@@ -3,19 +3,18 @@ import * as S from './styles'
 
 
 export const LandingPage = () => <S.Container>
-    <main>
-        <div className='Content'>
-            <div className ='TextBox'>
-                <h1>Título aqui, tudo o que você precisa.</h1>
-                <h2>Subtítulo logo abaixo pra te explicar melhor o que é isto.</h2>
-            </div>
-            <div className='InputCEP'>
-                <input type='text' required placeholder='Insira o endereço de entrega'/>
-            </div>
-            <div className ='CTA'>
-                <button>Buscar</button>
-            </div>
-        </div>
-    </main>
+
+    <S.Content>
+        <S.TextBox>
+            <h1>Título aqui, tudo o que você precisa.</h1>
+            <h2>Subtítulo logo abaixo pra te explicar melhor o que é isto.</h2>
+        </S.TextBox>
+        <S.InputCEP>
+            <input type='text' required placeholder='Insira o endereço de entrega'/>
+        </S.InputCEP>
+        <S.CTA>
+            <button>Buscar</button>
+        </S.CTA>
+    </S.Content>
 
 </S.Container>
--- src/pages/landing-page/styles.ts
+++ src/pages/landing-page/styles.ts
@@ -1,23 +1,62 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-
-main {
-    align-items: center;
-    align-content: center;
+    height: 100%;
+    display: flex;
     justify-content: center;
-}
+`;
 
-.Content {
-    display: grid;
-    grid-template-rows: 1fr 1fr 1fr;
+export const Content = styled.div`
+    padding: 0 10%;
+    display: flex;
+    flex-direction: column;
     align-items: center;
-    justify-items: center;
-}
+    justify-content: center;
+    gap: 50px;
+`;
 
-.TextBox {
+export const TextBox = styled.div`
     display:flex;
     flex-direction: column;
     text-align: center;
-}
+    gap: 15px;
+
+    h1 {
+        font-size: 1.8rem;
+        letter-spacing: 1px;
+        font-weight: 700;
+    }
+
+    h2 {
+        font-size: 1.2rem;
+        letter-spacing: 1px;
+        font-weight: 500;
+    }
+`;
+
+export const InputCEP = styled.div`
+    input {
+        text-align: center;
+        width: clamp(270px, 80vw, 400px);
+        height: 25px;
+        border-radius: 10px;
+    }
+
+    input::placeholder {
+        text-align: center;
+        letter-spacing: 1px;
+    }
+`;
+
+export const CTA = styled.div`
+    button {
+        color: white;
+        letter-spacing: 3px;
+        font-weight: 700;
+        width: clamp(100px, 40vw, 140px);
+        height: 25px;
+        background-color: red;
+        border: 0px;
+        border-radius: 10px;
+    }
 `;
