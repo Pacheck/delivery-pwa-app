6a6d2a580e21b0234c9bfb8f0d91faed59755858
Pedro Pacheco <pacheckdev@gmail.com>
2022-01-22 02:55:34 -0300

    adding redux provider, creating user-info reducer

---
+++ src/redux/reducers/user-info.reducer.ts
@@ -0,0 +1,46 @@
+import { SetUserName } from '../actions/user-info.action';
+
+type Order = {
+    id: string;
+    name: string;
+    price: number;
+    quantity: number;
+    deliveryTime: number;
+};
+
+type UserInformation = {
+    name: string;
+    cpf: string;
+    address: Object;
+    payment: Object;
+    orders: Array<Order>
+}
+const initialState: UserInformation = {
+    name: '',
+    cpf: '',
+    address: {},
+    payment: {},
+    orders: [],
+}
+export const UserInfoReducer = (action: any, state = initialState) => {
+    switch (action.type) {
+        case "SET_USER_NAME":
+            return { ...state, name: action.payload }
+        case "SET_USER_ADDRESS":
+            return { ...state, ...action.payload }
+        case "SET_USER_CPF":
+            return { ...state, cpf: action.payload }
+        case "SET_USER_PAYMENT":
+            return { ...state, payment: action.payload }
+        case "SET_USER_ORDERS":
+            const orderIndex = state.orders.findIndex(order => order.id === action.payload.id);
+            if (orderIndex > -1) {
+                const newOrders = [...state.orders, { ...state.orders[orderIndex], quantity: state.orders[orderIndex].quantity + 1 }];
+                return { ...state, order: newOrders }
+            }
+
+            return { ...state, orders: [...state.orders, action.payload] }
+        default:
+            return state;
+    }
+} 
