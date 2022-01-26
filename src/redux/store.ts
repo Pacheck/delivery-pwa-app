213c1c32d83d1c9efc9c843c294ef090898287f7
Pedro Pacheco <pacheckdev@gmail.com>
2022-01-26 03:34:01 -0300

    adding reducers to root reducer and state type

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,6 +1,17 @@
 import { combineReducers, createStore } from 'redux';
+import { ThemeReducer } from './reducers/theme.reducer';
 
-const rootReducer = combineReducers({})
+import { UserInfoReducer } from './reducers/user-info.reducer';
+import { UserOrderReducer } from './reducers/user-orders.reducer';
+
+const rootReducer = combineReducers({
+    theme: ThemeReducer,
+    userInfo: UserInfoReducer,
+    userOrder: UserOrderReducer,
+})
+
+export type RootReducerType = ReturnType<typeof rootReducer>
+
 
 const store = createStore(rootReducer);
 
