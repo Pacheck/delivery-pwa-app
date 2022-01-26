2f9aa87b34e8c3a1a801615fefcc9835cf237be3
Pedro Pacheco <pacheckdev@gmail.com>
2022-01-26 03:33:33 -0300

    creating user reducer and user order reducer also it actions

--- src/redux/reducers/user-info.reducer.ts
+++ src/redux/reducers/user-info.reducer.ts
@@ -4,29 +4,15 @@ import { UserInfoActionType, ActionType } from '../actions/user-info.action';
 const initialState: UserInformation = {
     name: '',
     cpf: '',
-    address: {},
-    payment: {},
-    orders: [],
 }
-export const UserInfoReducer = (action: ActionType, state = initialState) => {
+
+export const UserInfoReducer = (state = initialState, action: ActionType) => {
     switch (action.type) {
         case UserInfoActionType.SET_USER_NAME:
             return { ...state, name: action.payload }
-        case UserInfoActionType.SET_USER_ADDRESS:
-            return { ...state, ...action.payload }
         case UserInfoActionType.SET_USER_CPF:
             return { ...state, cpf: action.payload }
-        case UserInfoActionType.SET_USER_PAYMENT:
-            return { ...state, payment: action.payload }
-        case UserInfoActionType.SET_USER_ORDERS:
-            const orderIndex = state.orders.findIndex(order => order.id === action.payload.id);
-            if (orderIndex > -1) {
-                const newOrders = [...state.orders, { ...state.orders[orderIndex], quantity: state.orders[orderIndex].quantity + 1 }];
-                return { ...state, order: newOrders }
-            }
-
-            return { ...state, orders: [...state.orders, action.payload] }
         default:
             return state;
     }
-} 
+};
