b1610184f54d0be59eafc106b7f86f827577a923
Rianfiore <rianfiore@gmail.com>
2022-01-27 01:30:12 -0300

    add style for theme button

--- src/components/nav-bar/index.tsx
+++ src/components/nav-bar/index.tsx
@@ -1,13 +1,19 @@
 import React from 'react'
+import lightMode from '../../assets/lightMode.svg'
+import darkMode from '../../assets/darkMode.svg'
 import * as S from './styles'
 
 
 export const NavBar = () => <S.Container>
     <S.ThemeMode>
-    <label>
-        <span>Dark Mode</span>
-        <input type="checkbox"/>
-    </label>
+        <label>
+            <div>
+                <img src ={darkMode}/>
+                <img src ={lightMode}/>
+            </div>
+            <input type="checkbox"/>
+            <span></span>
+        </label>
     </S.ThemeMode>
 
 </S.Container>
